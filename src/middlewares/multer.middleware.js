2a1769f4bd92658637e8770216a835726456348c
SandeepShakya <sandeepshakya2015@gmail.com>
2025-02-20 20:38:22 +0530

    adding middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,19 @@
 import path from "path";
 import multer from "multer";
 
+// Configure storage settings for multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    // Set the destination folder for uploaded files
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
+    // Generate a unique filename using timestamp and random number
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     const ext = path.extname(file.originalname); // Extract the original file extension
     cb(null, file.fieldname + "-" + uniqueSuffix + ext);
   },
 });
 
+// Export the multer upload middleware with configured storage
 export const upload = multer({ storage: storage });
