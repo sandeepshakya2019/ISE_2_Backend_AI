fbbe53585c4abf5c68b9dfd0153ffe9a830a84ac
SandeepShakya <sandeepshakya2015@gmail.com>
2025-02-20 20:25:48 +0530

    changing app.js files

--- src/app.js
+++ src/app.js
@@ -4,30 +4,38 @@ import cors from "cors";
 
 const app = express();
 
+// Enable Cross-Origin Resource Sharing (CORS) to allow requests from different origins
 app.use(cors());
+
+// Parse incoming JSON requests with a size limit of 256kb
 app.use(
   express.json({
     limit: "256kb",
   })
 );
-app.use(express.urlencoded({ extended: true, limit: "256kb" }));
-app.use(express.static("public"));
-app.use(cookieParser());
-// (err, req, res, next)
 
-// import routes
+// Parse URL-encoded requests with extended mode and a size limit of 256kb
+app.use(express.urlencoded({ extended: true, limit: "256kb" }));
+
+// Serve static files from the "public" directory
+app.use(express.static("public"));
+
+// Enable cookie parsing middleware
+app.use(cookieParser());
+
+// Import route handlers
 import userRouter from "./routes/user.route.js";
 import validateRouter from "./routes/validate.route.js";
 import loanRoute from "./routes/loan.route.js";
 import profileRoute from "./routes/profile.route.js";
 
-app.use("/api/v1/users", userRouter);
+// Define API routes
+app.use("/api/v1/users", userRouter); // User-related routes
+app.use("/api/v1/validate", validateRouter); // Validation-related routes
+app.use("/api/v1/loan", loanRoute); // Loan-related routes
+app.use("/api/v1/profile", profileRoute); // Profile-related routes
 
-app.use("/api/v1/validate", validateRouter);
-
-app.use("/api/v1/loan", loanRoute);
-app.use("/api/v1/profile", profileRoute);
-
+// Uncomment the following line if finance-related routes need to be added
 // app.use("/api/v1/finance", validateRouter);
 
 export { app };
