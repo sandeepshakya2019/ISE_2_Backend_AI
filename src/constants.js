29afc83a9ba3eaaafc5298e5a3cab3011e37ff0a
SandeepShakya <sandeepshakya2015@gmail.com>
2025-02-20 20:22:51 +0530

    adding cinstat files

---
+++ src/constants.js
@@ -0,0 +1,56 @@
+export const DB_NAME = "APPDBANME";
+
+export const totalLoans = 3;
+
+export const dummmyAadharData = [
+  {
+    name: "Sandeep Kumar",
+    aadharNo: 212356565623,
+  },
+  {
+    name: "Sandeep Kumar",
+    aadharNo: 212356565623,
+  },
+  {
+    name: "Sandeep Kumar",
+    aadharNo: 212356565623,
+  },
+  {
+    name: "Sandeep Kumar",
+    aadharNo: 212356565623,
+  },
+  {
+    name: "Sandeep Kumar",
+    aadharNo: 212356565623,
+  },
+];
+
+export const dummmyBankData = [
+  {
+    accountHolderName: "Sandeep Kumar",
+    accountNumber: 56235623562356,
+    ifscCode: "SBIN000123",
+  },
+  {
+    accountHolderName: "Sandeep Kumar",
+    accountNumber: 56235623562356,
+    ifscCode: "SBIN000123",
+  },
+];
+
+export const dummmyRationData = [
+  {
+    rationCardHolderName: "Sandeep Kumar",
+    rationNumber: 56235623562356,
+  },
+  {
+    rationCardHolderName: "Sandeep Kumar",
+    rationNumber: 56235623562356,
+  },
+];
+
+export const dummmyincomeData = [
+  {
+    incomeCertificateNumber: 563256236523,
+  },
+];
