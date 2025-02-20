d54a11a1ad8bd9ab65279ea944b3f2cfb384fef5
SandeepShakya <sandeepshakya2015@gmail.com>
2025-02-20 20:37:20 +0530

    CHanges in index.js fil edb file

--- src/db/index.js
+++ src/db/index.js
@@ -4,14 +4,18 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const databaseInstance = await mongoose.connect(
-      `${process.env.DATABASE_URL}/${DB_NAME}`
+      `${process.env.DATABASE_URL}/${DB_NAME}`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
     );
     console.log(
       `[+] Connected to MongoDB (Host): ${databaseInstance.connection.host}`
     );
   } catch (err) {
-    console.error("[-] MongoDB Error", err);
-    process.exit(1);
+    console.error("[-] MongoDB Connection Error:", err.message);
+    process.exit(1); // Exit the process with failure
   }
 };
 
